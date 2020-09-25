ba6985af5584baae5564cc2eb02d43bfae61b8a2
basivii <kelam.basavaiah@kanini.com>
2020-09-25 18:49:04 +0530

    Setup project

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ViewBooksComponent } from './Components/view-books/view-books.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:"ViewBooks",component:ViewBooksComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
