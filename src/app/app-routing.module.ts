c00231c412a71338178722d5463ab5a487d3263a
janashika <57354275+janashika@users.noreply.github.com>
2020-09-30 13:26:23 +0530

    add book component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AddBookComponent } from './Components/add-book/add-book.component';
 import { EditViewComponent } from './Components/edit-view/edit-view.component';
 import { HomeComponent } from './Components/home/home.component';
 import { ViewBooksComponent } from './Components/view-books/view-books.component';
@@ -8,6 +9,7 @@ import { ViewBooksComponent } from './Components/view-books/view-books.component
 const routes: Routes = [
   {path:"ViewBooks",component:ViewBooksComponent},
   {path:"home",component:HomeComponent},
+  {path:"addBook",component:AddBookComponent},
   {path:"editView/:id",component:EditViewComponent},
   {path:'',component:HomeComponent}
 ];
