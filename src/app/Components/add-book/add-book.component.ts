6a4a653107e1edfbaddff6c2b09bf3f5404be503
janashika <57354275+janashika@users.noreply.github.com>
2020-10-05 15:45:05 +0530

    Update add-book.component.ts

--- src/app/Components/add-book/add-book.component.ts
+++ src/app/Components/add-book/add-book.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { BookDetails } from '../../Entities/book-details';
 import { BookService } from '../../Services/book.service';
 
@@ -11,7 +12,7 @@ export class AddBookComponent implements OnInit {
 
   book:BookDetails;
   result:string;
-  constructor(private bookService:BookService) { 
+  constructor(private bookService:BookService,private router:Router) { 
     this.book=new BookDetails();
   }
   insertBook(){
@@ -19,6 +20,7 @@ export class AddBookComponent implements OnInit {
     console.log(this.book);
     if(this.bookService.addBook(this.book)){
       this.result="Book added";
+      this.router.navigateByUrl("/ViewBooks");
     }
     else
       this.result="some error ";
