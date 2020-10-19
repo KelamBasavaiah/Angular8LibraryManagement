1db8436d58ce14346b9c578161262b5fdc959553
SMarimuthu13 <57354218+SMarimuthu13@users.noreply.github.com>
2020-10-19 20:33:22 +0530

    changes in my books

--- src/app/Components/my-books/my-books.component.ts
+++ src/app/Components/my-books/my-books.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { User } from 'src/app/Entities/user';
 
 @Component({
   selector: 'app-my-books',
@@ -6,10 +7,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./my-books.component.css']
 })
 export class MyBooksComponent implements OnInit {
+  userName:string = '';
+  userBooks:User[]=[];
 
   constructor() { }
 
+  getBooksforUser(userName){
+
+  }
+
   ngOnInit() {
+    this.getBooksforUser(this.userName)
   }
 
 }
