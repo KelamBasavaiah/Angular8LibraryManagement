ce4def11e7a41647643a08bf66318d01de3fea9c
SMarimuthu13 <57354218+SMarimuthu13@users.noreply.github.com>
2020-10-20 16:31:11 +0530

    updates of my books

--- src/app/Components/my-books/my-books.component.ts
+++ src/app/Components/my-books/my-books.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ToastrService } from 'ngx-toastr';
 import { User } from 'src/app/Entities/user';
 import { LoginService } from 'src/app/Services/login.service';
 
@@ -11,14 +12,21 @@ export class MyBooksComponent implements OnInit {
   userName:string = 'Mari';
   userBooks:User[]=[];
 
-  constructor(private loginService:LoginService) { }
+  constructor(private loginService:LoginService,private toastr:ToastrService) { }
 
   getBooksforUser(userName){
     this.loginService.getBooksforUser(userName).subscribe((data:any[])=>{this.userBooks=data});  
   }
 
-  retrunBook(book){
-
+  returnBook(book){
+    console.log(book);
+    this.loginService.returnBook(book).subscribe((data:any)=>{
+      if(data){
+        this.toastr.success('Book Returned Successfully!', 'Success!');
+      }else{
+        this.toastr.warning('something went wrong!', 'Failed!');
+      }
+    })
   }
 
   ngOnInit() {
