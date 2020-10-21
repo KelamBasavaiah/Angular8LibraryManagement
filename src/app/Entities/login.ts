f0092643530ce26f6fd79e009c55c9acb11548c1
basivii <kelam.basavaiah@kanini.com>
2020-10-21 12:45:25 +0530

    login user id

---
+++ src/app/Entities/login.ts
@@ -0,0 +1,14 @@
+export class Login {
+    userId:number;
+    username:string;
+    password:string;
+    role:number;
+    aurthorize:boolean
+    constructor(userId?,username?,password?,role?,aurthorize?){
+        this.userId=userId;
+        this.username=username;
+        this.password=password;
+        this.role = role;
+        this.aurthorize=aurthorize;
+    }
+}
